ca45f9ba176280c0db0f6cc25a9a33b8126062b8
Claude Henchoz <claude.henchoz@gmail.com>
2013-08-01 23:48:15 +0200

    Added ch-xtools.psm1

---
+++ ch-xtools.psm1
@@ -0,0 +1,4 @@
+function Measure-100Commands ($command) {
+    # Runs a command 100 times and measures the time it takes to execute it
+    1..100 | foreach {Measure-Command -Expression {Invoke-Expression $command}} | Measure-Object -Property TotalMilliseconds -Average
+}
