6c2877590c266d97c5cba206be37210687a5f7cb
Claude Henchoz <claude.henchoz@gmail.com>
2013-08-01 23:52:07 +0200

    Line breaks

--- ch-xtools.psm1
+++ ch-xtools.psm1
@@ -1,4 +1,5 @@
 function Measure-100Commands ($command) {
     # Runs a command 100 times and measures the time it takes to execute it
-    1..100 | foreach {Measure-Command -Expression {Invoke-Expression $command}} | Measure-Object -Property TotalMilliseconds -Average
+    1..100 | foreach {Measure-Command -Expression {Invoke-Expression $command}} |
+             Measure-Object -Property TotalMilliseconds -Average
 }
