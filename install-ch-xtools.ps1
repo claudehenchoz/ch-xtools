0b1fb310581796d50d462eba306275ea77305556
Claude Henchoz <claude.henchoz@gmail.com>
2014-12-25 00:07:53 +0100

    Added loading of the new module to avoid having to (re-) start PS

--- install-ch-xtools.ps1
+++ install-ch-xtools.ps1
@@ -2,4 +2,5 @@ $url = "https://raw.githubusercontent.com/claudehenchoz/ch-xtools/master/ch-xtoo
 $chxtoolspath = [environment]::getfolderpath("mydocuments") + "\WindowsPowerShell\Modules\ch-xtools"
 if (!(Test-Path $chxtoolspath)) { New-Item -itemtype "Directory" $chxtoolspath -force | Out-Null }
 Invoke-WebRequest $url -OutFile "$chxtoolspath\ch-xtools.psm1" 
-Write-Output "Done. Please (re-) start PowerShell."
+Import-Module "$chxtoolspath\ch-xtools.psm1"
+Write-Output "Done."
