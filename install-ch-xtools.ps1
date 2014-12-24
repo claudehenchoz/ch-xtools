c2bfab6a577112891112b46d298113e1ea101723
Claude Henchoz <claude.henchoz@gmail.com>
2014-12-25 00:25:27 +0100

    Added a line of advice to the end of the install

--- install-ch-xtools.ps1
+++ install-ch-xtools.ps1
@@ -3,4 +3,4 @@ $chxtoolspath = [environment]::getfolderpath("mydocuments") + "\WindowsPowerShel
 if (!(Test-Path $chxtoolspath)) { New-Item -itemtype "Directory" $chxtoolspath -force | Out-Null }
 Invoke-WebRequest $url -OutFile "$chxtoolspath\ch-xtools.psm1" 
 Import-Module "$chxtoolspath\ch-xtools.psm1"
-Write-Output "Done."
+Write-Output "Done. Run `"Get-Command -Module ch-xtools`" to get a list of tools."
