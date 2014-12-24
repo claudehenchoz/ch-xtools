d4ce1cf15c46ab1c1419368a0b29c6ee84d509bf
Claude Henchoz <claude.henchoz@gmail.com>
2014-12-24 06:56:42 +0100

    Made folder creation silent Added message at the end

--- install-ch-xtools.ps1
+++ install-ch-xtools.ps1
@@ -1,4 +1,5 @@
 $url = "https://raw.githubusercontent.com/claudehenchoz/ch-xtools/master/ch-xtools.psm1"
 $chxtoolspath = [environment]::getfolderpath("mydocuments") + "\WindowsPowerShell\Modules\ch-xtools"
-if (!(Test-Path $chxtoolspath)) { New-Item -itemtype "Directory" $chxtoolspath -force }
+if (!(Test-Path $chxtoolspath)) { New-Item -itemtype "Directory" $chxtoolspath -force | Out-Null }
 Invoke-WebRequest $url -OutFile "$chxtoolspath\ch-xtools.psm1" 
+Write-Output "Done. Please (re-) start PowerShell."
