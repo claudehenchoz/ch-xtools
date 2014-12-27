e425ade3e250e62cb7aa0fa1e9edba031a1bce6f
Claude Henchoz <claude.henchoz@gmail.com>
2014-12-27 18:01:27 +0100

    Shortened post-install instructions Split one long line into two

--- install-ch-xtools.ps1
+++ install-ch-xtools.ps1
@@ -5,7 +5,8 @@ $url = "https://raw.githubusercontent.com/claudehenchoz/ch-xtools/master/ch-xtoo
 $modname = (([System.Uri]"$url").Segments[-1]).Split(".")[0]
 
 # Create local module path
-$modpath = [environment]::getfolderpath("mydocuments") + "\WindowsPowerShell\Modules\$modname"
+$modpath = [environment]::getfolderpath("mydocuments") + `
+               "\WindowsPowerShell\Modules\$modname"
 
 # Create module folder(s) if it doesn't exist
 if (!(Test-Path $modpath)) {
@@ -17,4 +18,4 @@ Invoke-WebRequest $url -OutFile "$modpath\$modname.psm1"
 
 # Import so it becomes immediately loaded
 Import-Module "$modpath\$modname.psm1"
-Write-Output "Done. Run `"Get-Command -Module $modname`" to get a list of tools."
+Write-Output "Done. Run `"gcm -m $modname`" to get a list of tools."
