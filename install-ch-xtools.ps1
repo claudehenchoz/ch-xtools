bf7710f346c88563596d4c46ff29ba1831999e2c
Claude Henchoz <claude.henchoz@gmail.com>
2014-12-28 04:50:38 +0100

    Made final install report list URL/path Rewrote installer as function

--- install-ch-xtools.ps1
+++ install-ch-xtools.ps1
@@ -1,21 +1,30 @@
 # URL to .psm1 file
-$url = "https://raw.githubusercontent.com/claudehenchoz/ch-xtools/master/ch-xtools.psm1"
+$Url = "https://raw.githubusercontent.com/claudehenchoz/ch-xtools/master/ch-xtools.psm1"
 
-# File name of URL without extension
-$modname = (([System.Uri]"$url").Segments[-1]).Split(".")[0]
+function Install-ModuleFromUri {
+    Param([Parameter(Mandatory=$true,Position=0)][string]$Uri)
 
-# Create local module path
-$modpath = [environment]::getfolderpath("mydocuments") + `
-               "\WindowsPowerShell\Modules\$modname"
+    # File name of URL without extension
+    $modname = (([System.Uri]"$Uri").Segments[-1]).Split(".")[0]
 
-# Create module folder(s) if it doesn't exist
-if (!(Test-Path $modpath)) {
-    New-Item -itemtype "Directory" $modpath -force | Out-Null
+    # Create local module path
+    $modpath = [environment]::getfolderpath("mydocuments") + `
+                   "\WindowsPowerShell\Modules\$modname"
+
+    # Create module folder(s) if it doesn't exist
+    if (!(Test-Path $modpath)) {
+        New-Item -itemtype "Directory" $modpath -force | Out-Null
+    }
+
+    # Download module
+    Invoke-WebRequest $Uri -OutFile "$modpath\$modname.psm1"
+
+    # Import so it becomes immediately loaded
+    Import-Module "$modpath\$modname.psm1"
+    Write-Output "Done installing $modname!`n"
+    Write-Output "Installed from: $Uri"
+    Write-Output "Installed to:   $modpath`n"
+    Write-Output "Run `"gcm -m $modname`" to get a list of features."
 }
 
-# Download module
-Invoke-WebRequest $url -OutFile "$modpath\$modname.psm1"
-
-# Import so it becomes immediately loaded
-Import-Module "$modpath\$modname.psm1"
-Write-Output "Done. Run `"gcm -m $modname`" to get a list of tools."
+Install-ModuleFromUri -Uri "$Url"
